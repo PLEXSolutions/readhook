de08442b6c2298951738fc61d1ee73afbffd0ccb
rsundahl <roy@polyverse.io>
2017-12-28 10:29:11 -0800

    Manage strings properly using const.

--- redhook.c
+++ redhook.c
@@ -4,7 +4,17 @@
 #include <stdio.h>
 #include <string.h>
 
-static char *magic = "xyzzy";
+static const char *elf_signature = "\0x7fELF";
+
+static const char *magic = "xyzzy";
+
+static const char *s_disclose = "DISCLOSE";
+static const char *s_overflow = "OVERFLOW";
+
+static const char *s_libc_base       = "base";
+static const char *s_libc_mprotect   = "mprotect";
+static const char *s_libc_read       = "read";
+static const char *s_libc_start_main = "__libc_start_main";
 
 /* Function pointers to hold the value of the glibc functions */
 static ssize_t (*real_read)(int fd, const void *buf, size_t count) = NULL;
@@ -12,7 +22,7 @@ static ssize_t (*real_read)(int fd, const void *buf, size_t count) = NULL;
 static void *find_libc_base(void *p)
 {
 	p = (void *) (((unsigned long) p) & 0xfffffffffffff000);
-	while (strncmp(p, "\0x7fELF", 4))
+	while (strncmp(p, elf_signature, strlen(elf_signature)))
 		p -= 0x1000;
 
 	return p;
@@ -20,9 +30,10 @@ static void *find_libc_base(void *p)
 
 static void disclose(void)
 {
-	void *libc_mprotect   = dlsym(RTLD_NEXT, "mprotect");
-	void *libc_read       = dlsym(RTLD_NEXT, "read");
-	void *libc_start_main = dlsym(RTLD_NEXT, "__libc_start_main");
+	void *libc_mprotect   = dlsym(RTLD_NEXT, s_libc_mprotect);
+	void *libc_read       = dlsym(RTLD_NEXT, s_libc_read);
+	void *libc_start_main = dlsym(RTLD_NEXT, s_libc_start_main);
+
 	void *libc_base       = find_libc_base(libc_mprotect);
 
 	ptrdiff_t libc_mprotect_offset   = libc_mprotect - libc_base;
@@ -30,10 +41,10 @@ static void disclose(void)
 	ptrdiff_t libc_start_main_offset = libc_start_main - libc_base;
 
 	printf("--------------------------------------------\n");
-	printf("libc_base:       %p\n",           libc_base);
-	printf("libc_mprotect:   %p (0x%08tx)\n", libc_mprotect, libc_mprotect_offset);
-	printf("libc_read:       %p (0x%08tx)\n", libc_read, libc_read_offset);
-	printf("libc_start_main: %p (0x%08tx)\n", libc_start_main, libc_start_main_offset);
+	printf("%20s: %p\n",           s_libc_base,       libc_base);
+	printf("%20s: %p (0x%08tx)\n", s_libc_mprotect,   libc_mprotect,   libc_mprotect_offset);
+	printf("%20s: %p (0x%08tx)\n", s_libc_read,       libc_read,       libc_read_offset);
+	printf("%20s: %p (0x%08tx)\n", s_libc_start_main, libc_start_main, libc_start_main_offset);
 	printf("--------------------------------------------\n");
 } // reconnaissance()
 
@@ -48,18 +59,17 @@ static void overflow(char *src, size_t n)
 
 ssize_t read(int fd, const void *buf, size_t count)
 {
-	//printf("read:chars#:%lu\n", count);
-	real_read = dlsym(RTLD_NEXT, "read");
+	if (!real_read)
+		real_read = dlsym(RTLD_NEXT, s_libc_read);
 	ssize_t result = real_read(fd, buf, count);
-	//printf("real_read:chars#:%lu\n", result);
 	char *p = (result < strlen(magic)) ? NULL : strstr(buf, magic);
 
 	if (p)
 	{
 		p += strlen(magic);
-		if (!strncmp("DISCLOSE", p, strlen("DISCLOSE")))
+		if (!strncmp(s_disclose, p, strlen(s_disclose)))
 			disclose();
-		else if (!strncmp("OVERFLOW", p, strlen("OVERFLOW")))
+		else if (!strncmp(s_overflow, p, strlen(s_overflow)))
 			overflow(p, result - (p - (char *) buf));
 	} // if
 
