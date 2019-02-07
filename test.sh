68dbe4058de39005b907423d3a376317126331f9
rsundahl <roy@polyverse.io>
2019-02-07 11:51:18 -0800

    Don't need to map ports for test.sh when only need egress.

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ if [[ "$hostport" == "" ]]; then
 	hostport="docker.for.mac.localhost:5555"
 fi
 
-docker run -it --rm --name readhook -p 5555:5555 readhook /readhook/app/fullhook "$hostport"
+docker run -it --rm --name readhook readhook /readhook/app/fullhook "$hostport"
